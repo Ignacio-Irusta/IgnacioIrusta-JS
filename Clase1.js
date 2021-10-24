b433b859af552f04a8c91a2cec571642dc1dc329
Ignacio Irusta <ignacio_r99@hotmail.com>
2021-10-24 20:22:45 +0200

    Ejercicio 1 y 2 en el mismo JS

---
+++ Clase1.js
@@ -0,0 +1,20 @@
+let notauno = prompt( "Ingrese la nota del primer trimestre" );
+let notados = prompt( "Ingrese la nota del segundo trimestre" );
+let notatres = prompt( "Ingrese la nota del tercer trimestre" );
+
+suma = parseInt(notauno) + parseInt(notados) + parseInt(notatres);
+notafinal = suma / 3;
+
+if(notafinal >= 7){
+
+    console.log( "Usted aprobo la materia con: " + notafinal);
+
+} else if(notafinal >= 4){
+
+    console.log( "Necesita clases de recuperacion." );
+
+} else {
+
+    alert( "Usted desaprobo :,(" );
+
+}
